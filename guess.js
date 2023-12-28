0ed0ba144ff63ad70f7e9398a76887d3a6fe208b
Ashraf Alagmawy <ashrafemad097@gmail.com>
2023-12-28 17:26:48 +0200

    First Version

---
+++ guess.js
@@ -0,0 +1,11 @@
+// Setting Game Name
+
+let gameName = 'Guess The Word';
+
+document.title = gameName;
+
+document.querySelector('h1').innerHTML = gameName;
+
+document.querySelector(
+  'footer'
+).innerHTML = `${gameName} Game &copy; Created By Ashraf Alagmawy`;
